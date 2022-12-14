48b9535480a6e0d57eef87f8ab92d78414cd7105
Yang <hidorabmon@gmail.com>
2022-12-15 09:52:54 +1100

    first update

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+function submitData(name, email) {
+  console.log("call submitData");
+  const user = {
+    name: name,
+    email: email,
+  };
+
+  const configuration = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+
+return  fetch(" http://localhost:3000/users", configuration)
+    .then((res) => res.json())
+    .then((userData) => renderUser(userData))
+    .catch((error) => errorInfo(error));
+}
+
+function renderUser(userData) {
+  console.log("call renderUser");
+
+  const li = document.createElement("li");
+  const text = `User Name = ${userData.name}
+       Email ＝ ${userData.email}
+       ID = ${userData.id}`;
+  li.innerText = text
+  document.querySelector("#users").appendChild(li);
+}
+
+function errorInfo(messenge) {
+  document.querySelector("#errorInfo").innerHTML = messenge;
+}
